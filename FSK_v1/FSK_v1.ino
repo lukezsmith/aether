4642755fd1ca5503127be15d95fbb87d3f0ee9f8
lukezsmith <lukezachsmith@gmail.com>
2021-04-01 17:10:42 +0100

    updateDataString function implemented dataStringHeader (currently just the callsign) implemented

--- FSK_v1/FSK_v1.ino
+++ FSK_v1/FSK_v1.ino
@@ -5,27 +5,33 @@
 // Pin Definitions
 #define TXPIN PB5 // Radio TX Pin
 
+// General Variables
+const char callSign[] = "<AETHER> ";
+
 // Transmission Variables
-const int dataStringLength = 80;  // max length of TX datastring
-//char datastring[2][dataStringLength];
-char datastring[dataStringLength];  // TX datastring
-uint8_t txByte = 0; // datastring current byte index indicator
+const int dataStringLength = 100;  // max length of TX dataString
+//char dataString[2][dataStringLength];
+char dataString[dataStringLength];  // TX dataString
+char dataStringHeader[18];  // header substring for TX dataString
+uint8_t txByte = 0; // dataString current byte index indicator
 uint8_t txBit = 0; // txByte current bit indicator
-char currentByte = '$'; // current datastring byte
+char currentByte = '$'; // current dataString byte
 boolean stopBit = false; // Stop bit transmission state indicator
 boolean startBit = true; // Start bit transmission state indicator
 boolean dataBit = false; // Data bit transmission state indicator
-boolean newByte = true; // new byte from datastring indicator
+boolean newByte = true; // new byte from dataString indicator
 
 // Interrupt Counter and Compare Values
 const uint16_t t1Load = 0;
 const uint16_t t1Comp = 5000;  // (0.02s * 16MHz) / 64 = 5000 (below Timer1 max val of 65535) =>  0.02s/20ms per interrupt
 
 void setup() {
-
   // Setup Serial monitoring
   Serial.begin(9600);
 
+  // Set header string to callsign
+  sprintf(dataStringHeader, "%s", callSign);
+  
   // Clear all interrupts
   cli();
 
@@ -55,16 +61,7 @@ void setup() {
 }
 
 void loop() {
-  //  char tempData[dataStringLength];
-  //  sprintf(tempData,"RTTY TEST BEACON RTTY TEST BEACON\n"); // Puts the text in the datastring
-  //  unsigned int CHECKSUM = gps_CRC16_checksum(datastring);  // Calculates the checksum for this datastring
-  //  char checksum_str[6];
-  //  sprintf(checksum_str, "*%04X\n", CHECKSUM);
-  //  strcat(tempData,checksum_str);
-  cli();
-  sprintf(datastring, "RTTY TEST BEACON RTTY TEST BEACON\n"); // Puts the text in the datastring
-  //  sprintf(datastring, tempData);
-  sei();
+  updatedataString();
 }
 
 ISR(TIMER1_COMPA_vect) {
@@ -73,21 +70,15 @@ ISR(TIMER1_COMPA_vect) {
   // if we are sending a new byte
   if (newByte) { 
     newByte = false;
-    //    currentByte = datastring[buffer][txByte];
-    currentByte = datastring[txByte];
+    //    currentByte = dataString[buffer][txByte];
+    currentByte = dataString[txByte];
     txByte++;
     // if we have reached the end of the string or buffer reset all byte state indicators
     if (currentByte == '\0' || txByte > (dataStringLength - 1)) {  
       //      buffer = !buffer; // switch buffers
       txByte = 0;
       currentByte = 0x00;
-//      nullByte = true;
     }
-//    if (nullByte) { // send a second null byte at start of string.
-//      currentByte = 0x00;
-//      txByte = 0;
-//      nullByte = false;
-//    }
     txBit = 0;
     startBit = true;
     dataBit = false;
@@ -130,10 +121,41 @@ ISR(TIMER1_COMPA_vect) {
       txBit = 0;
       dataBit = false;
     }
-
   }
-
 }
+
+// function to build the transmission dataString and store in buffer
+void updatedataString(){
+  char tempData[dataStringLength];
+  
+  // disable interrupts temporarily to stop partial transmissions
+  cli();
+  
+  // Construct header substring
+  sprintf(tempData, dataStringHeader);
+  
+  // Construct GPS substring
+  
+  // Construct temperature substring
+  
+  // Construct pressure substring 
+
+  // Construct checksum substring
+//  unsigned int CHECKSUM = gps_CRC16_checksum(dataString);  // Calculates the checksum for this dataString
+//  char checksum_str[6];
+//  sprintf(checksum_str, "*%04X\n", CHECKSUM);
+//  strcat(tempData,checksum_str);
+
+  // test dataString
+  strcat(tempData,"RTTY TEST BEACON RTTY TEST BEACON\n"); // Puts the text in the dataString
+
+  // Store string in dataString buffer
+  sprintf(dataString, tempData);
+  
+  // re-enable interrupts 
+  sei();
+}
+
 
 // function to generate a CRC checksum to be used in the transmission string
 uint16_t gps_CRC16_checksum (char *string)
